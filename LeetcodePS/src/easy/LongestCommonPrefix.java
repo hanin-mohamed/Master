344610f905bb2de71b838d7307e4c6f4d090dd8f
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-08-03 10:30:11 +0300

    Coding 😋

--- LeetcodePS/src/easy/LongestCommonPrefix.java
+++ LeetcodePS/src/easy/LongestCommonPrefix.java
@@ -1,36 +1,21 @@
-//package easy;
-//
-//import java.util.*;
-//
-//public class LongestCommonPrefix {
-//
-//
-//    public String longestCommonPrefix(String[] strs) {
-//        List<String> list = new ArrayList<String>();
-//        String s = strs[0], temp = "";
-//        boolean flag = false;
-//        for (int i = 0; i < s.length(); i++) {
-//            temp += s.charAt(i);
-//            for (int j = 0; j < strs.length; j++) {
-//                if (!strs[j].contains(temp)) {
-//                    flag = true;
-//                }
-//            }
-//            if (flag ==false) {
-//                list.add(temp);
-//            }
-//            else {temp=""; break;}
-//        }
-//
-//        for (int i = 0; i < list.size(); i++) {
-//            if (list.get(i).length() > temp.length());
-//
-//        }
-//        return temp;
-//    }
-//
-//    public static void main(String[] args) {
-//        LongestCommonPrefix lcp = new LongestCommonPrefix();
-//        System.out.println(lcp.longestCommonPrefix(new String[]{"flower","flow","flight"}));
-//    }
-//}
+package easy;
+
+import java.util.*;
+
+public class LongestCommonPrefix {
+
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null &&strs.length==0) return "";
+      Arrays.sort(strs);
+      String prefix = strs[0],end = strs[strs.length-1];
+      int i=0;
+      while(i<prefix.length()&&prefix.charAt(i)==end.charAt(i)){i++;}
+      return prefix.substring(0,i);
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
+        System.out.println(lcp.longestCommonPrefix(new String[]{"ab","a"}));
+    }
+}
--- LeetcodePS/src/easy/PlusOne.java
+++ LeetcodePS/src/easy/PlusOne.java
@@ -3,27 +3,22 @@ package easy;
 import java.util.Arrays;
 
 public class PlusOne {
-//    public int[] plusOne(int[] digits) {
-//        int n=digits.length;
-//        int[] newDigits = new int[n + 1];
-//        newDigits=digits;
-//        int max= Arrays.stream(digits).max().orElseThrow();
-//        max++;
-//        for (int i = 0; i < digits.length; i++) {
-//            if (newDigits[i]==max-1){
-//                if (max%10==0){
-//                    newDigits[i]=max%10;
-//                    newDigits [n-1]=0;
-//                }
-//                else newDigits[i]=max;
-//            }
-//
-//        }
-//        return newDigits;
-//    }
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for (int i = n-1; i >=0; i--) {
+            digits[i]++;
+                if (digits[i]<10){
+                   return digits;
+                }
+                digits[i]=0;
+            }
+        int nums[]= new int[n+1];
+        nums[0]=1;
+        return nums;
+    }
 
     public static void main(String args[]){
         PlusOne plusOne= new PlusOne();
-       // System.out.println(plusOne.plusOne(new int[]{4,5,6,6,9}));
+       System.out.println(plusOne.plusOne(new int[]{4,5,6,6,9}));
     }
 }
