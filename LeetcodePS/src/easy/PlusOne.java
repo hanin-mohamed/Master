23853aaf91ae0e2ed789cb827ca278a419477830
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-08-03 03:40:37 +0300

    Coding

---
+++ LeetcodePS/src/easy/PlusOne.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.Arrays;
+
+public class PlusOne {
+//    public int[] plusOne(int[] digits) {
+//        int n=digits.length;
+//        int[] newDigits = new int[n + 1];
+//        newDigits=digits;
+//        int max= Arrays.stream(digits).max().orElseThrow();
+//        max++;
+//        for (int i = 0; i < digits.length; i++) {
+//            if (newDigits[i]==max-1){
+//                if (max%10==0){
+//                    newDigits[i]=max%10;
+//                    newDigits [n-1]=0;
+//                }
+//                else newDigits[i]=max;
+//            }
+//
+//        }
+//        return newDigits;
+//    }
+
+    public static void main(String args[]){
+        PlusOne plusOne= new PlusOne();
+       // System.out.println(plusOne.plusOne(new int[]{4,5,6,6,9}));
+    }
+}
