788bd04bc80c758683c568e8e332e04dad684067
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-08-04 09:57:37 +0300

    Playing with Code😫

---
+++ LeetcodePS/src/easy/ShortestCmpltWord.java
@@ -0,0 +1,11 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShortestCmpltWord {
+
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+       
+    }
+}
