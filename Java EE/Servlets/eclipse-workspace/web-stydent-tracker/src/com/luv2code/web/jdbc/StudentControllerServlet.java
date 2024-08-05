36c60abdc130d84c84f94649f0869728b7b446d2
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-08-05 08:14:17 +0300

    Update Done🎉

--- Java EE/Servlets/eclipse-workspace/web-stydent-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
+++ Java EE/Servlets/eclipse-workspace/web-stydent-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -1,7 +1,6 @@
 package com.luv2code.web.jdbc;
 
 import java.util.List;
-
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,95 +10,97 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-/**
- * Servlet implementation class StudentControllerServlet
- */
 @WebServlet("/StudentControllerServlet")
 public class StudentControllerServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	private StudentDbUtil studentDbUtil;
-	@Resource(name="jdbc/web_student_tracker")
-	private DataSource dataSource;
-	
+    private static final long serialVersionUID = 1L;
+
+    private StudentDbUtil studentDbUtil;
+    @Resource(name = "jdbc/web_student_tracker")
+    private DataSource dataSource;
+
     @Override
-	public void init() throws ServletException {
-		super.init();
-		
-		try {
-			studentDbUtil = new StudentDbUtil(dataSource);
-		}
-		catch(Exception exc) {
-			throw new ServletException(exc);
-		}
-	}
+    public void init() throws ServletException {
+        super.init();
 
+        try {
+            studentDbUtil = new StudentDbUtil(dataSource);
+        } catch (Exception exc) {
+            throw new ServletException(exc);
+        }
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException{
-		
-		try {
-			String command=request.getParameter("command");
-			if(command==null)command="LIST";
-			
-			switch (command) {
-			case "LIST":
-				listStudents(request,response);	
-                 break;
-                 
-			case "ADD":
-				addStudent(request,response);
-				break;
-				
-			case "LOAD":
-				loadStudent(request,response);
-				break;
-				
-				default :
-					listStudents(request,response);	
-			}
-		} 
-		
-	   catch (Exception e) {
-       throw new ServletException(e);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
+            String command = request.getParameter("command");
+            if (command == null) command = "LIST";
 
-		}
-	
-	}
-   
+            switch (command) {
+                case "LIST":
+                    listStudents(request, response);
+                    break;
 
-	private void loadStudent(HttpServletRequest request, HttpServletResponse response) {
-		
-		String studentId=request.getParameter("studentId");
-		
-		Student student = studentDbUtil.getStudents(studentId);
-		request.setAttribute("THE_STUDENT", student);
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
-		dispatcher.forward(request,response);
-	}
-		
-	}
+                case "ADD":
+                    addStudent(request, response);
+                    break;
 
+                case "LOAD":
+                    loadStudent(request, response);
+                    break;
+                    
+                case "UPDATE":
+                    updateStudent(request, response);
 
-	private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		String firstName=request.getParameter("firstName");
-		String lastName=request.getParameter("lastName");
-		String email =request.getParameter("email");
-		
-		Student student=new Student(firstName,lastName,email);
-		studentDbUtil.addStudent(student);
-		
-		listStudents(request,response);
-	}
+                default:
+                    listStudents(request, response);
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+    
+    
 
+    private void updateStudent(HttpServletRequest request, HttpServletResponse response)
+    		throws Exception {
 
-	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		List<Student> students =studentDbUtil.getStudents();		
-		request.setAttribute("STUDENT_LIST", students);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
-		dispatcher.forward(request,response);
-	}
+    		// read student info from form data
+    		int id = Integer.parseInt(request.getParameter("studentId"));
+    		String firstName = request.getParameter("firstName");
+    		String lastName = request.getParameter("lastName");
+    		String email = request.getParameter("email");
+    		
+    		// create a new student object
+    		Student student = new Student(id, firstName, lastName, email);
+    		
+    		// perform update on database
+    		studentDbUtil.updateStudent(student);
+    		
+    		// send them back to the "list students" page
+    		listStudents(request, response);
+    		
+    	}
 
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String studentId = request.getParameter("studentId");
+        Student student = studentDbUtil.getStudent(studentId);
+        request.setAttribute("THE_STUDENT", student);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
+        dispatcher.forward(request, response);
+    }
 
+    private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        Student student = new Student(firstName, lastName, email);
+        studentDbUtil.addStudent(student);
+        listStudents(request, response);
+    }
+
+    private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<Student> students = studentDbUtil.getStudents();
+        request.setAttribute("STUDENT_LIST", students);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+        dispatcher.forward(request, response);
+    }
 }
--- Java EE/Servlets/eclipse-workspace/web-stydent-tracker/src/com/luv2code/web/jdbc/StudentDbUtil.java
+++ Java EE/Servlets/eclipse-workspace/web-stydent-tracker/src/com/luv2code/web/jdbc/StudentDbUtil.java
@@ -7,99 +7,116 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.sql.DataSource;
 
 public class StudentDbUtil {
-	private DataSource dataSource;
-	
-	public StudentDbUtil (DataSource dataSource)
-	{
-		this.dataSource =dataSource;
-	}
-	public  List<Student> getStudents() throws SQLException{
-		List <Student> students =new ArrayList<>();
-	     Connection con =null;
-	     Statement stmt = null;
-	     ResultSet rslt = null;
-	    
-	     try {
-	    	 
-	    	 con=dataSource.getConnection();
-	    	 String sql =" select * from student order by last_name";
-	    	 stmt =con.createStatement();
-	    	 rslt=stmt.executeQuery(sql);
-	    	 while (rslt.next()) {
-	    		 int id=rslt.getInt("id");
-	    		 String firstName= rslt.getString("first_name");
-	    		 String lastName= rslt.getString("last_name");
-	    		 String email = rslt.getString("email");
-	    		 Student tempStudent = new Student(id , firstName ,lastName,email);
-	    		 students.add(tempStudent);
-	    		 
-	    	 }
-	    	  return students;
-	 	}
-	     finally{
-	    	 //close jdbc
-	    	 close(con,stmt,rslt);
-	    	 
-	     }
-	}
-	private void close(Connection con, Statement stmt, ResultSet rslt) {
-		// TODO Auto-generated method stub
-		try {
-	if(rslt !=null) {
-		rslt.close();
-	}
-	if(stmt != null )
-	{
-		stmt.close();
-	}
-	if(con !=null) {
-		con.close();
-	}
-	
-	}
-		catch(Exception ex){
-			ex.printStackTrace();
-		}
-	
-	}
-	public void addStudent(Student student) throws SQLException {
-	Connection con =null;
-	PreparedStatement stmt=null;
-	try {
-		con=dataSource.getConnection();
-		String sql ="insert into student (first_name,last_name,email) values (?,?,?)";
-		stmt = con.prepareStatement(sql);
-		stmt.setString(1, student.getFirstName());
-		stmt.setString(2, student.getLastName());
-		stmt.setString(3, student.getEmail());
-		stmt.execute();
-	}
-	finally{
-		close(con,stmt,null);
-		
-	}
-	
-	
-	}
-	public Student getStudents(String studentId) {
-	Student student =null;
-	Connection con=null;
-	PreparedStatement stmt = null;
-	ResultSet rslt=null;
-	
-	try {
-		
-		
-		
-	return student;	
-	}
-	finally {
-		
-	}
-	}
-	
+    private DataSource dataSource;
+
+    public StudentDbUtil(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Student> getStudents() throws SQLException {
+        List<Student> students = new ArrayList<>();
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rslt = null;
+
+        try {
+            con = dataSource.getConnection();
+            String sql = "SELECT * FROM student ORDER BY last_name";
+            stmt = con.createStatement();
+            rslt = stmt.executeQuery(sql);
+
+            while (rslt.next()) {
+                int id = rslt.getInt("id");
+                String firstName = rslt.getString("first_name");
+                String lastName = rslt.getString("last_name");
+                String email = rslt.getString("email");
+                Student tempStudent = new Student(id, firstName, lastName, email);
+                students.add(tempStudent);
+            }
+            return students;
+        } finally {
+            close(con, stmt, rslt);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rslt) {
+        try {
+            if (rslt != null) {
+                rslt.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void addStudent(Student student) throws SQLException {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        try {
+            con = dataSource.getConnection();
+            String sql = "INSERT INTO student (first_name, last_name, email) VALUES (?, ?, ?)";
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, student.getFirstName());
+            stmt.setString(2, student.getLastName());
+            stmt.setString(3, student.getEmail());
+            stmt.execute();
+        } finally {
+            close(con, stmt, null);
+        }
+    }
+
+    public Student getStudent(String theStudentId) throws Exception {
+        Student student = null;
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rslt = null;
+        int studentId;
+
+        try {
+            studentId = Integer.parseInt(theStudentId);
+            con = dataSource.getConnection();
+            String sql = "SELECT * FROM student WHERE id=?";
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, studentId);
+            rslt = stmt.executeQuery();
+
+            if (rslt.next()) {
+                String firstName = rslt.getString("first_name");
+                String lastName = rslt.getString("last_name");
+                String email = rslt.getString("email");
+                student = new Student(studentId, firstName, lastName, email);
+            }
+            return student;
+        } finally {
+            close(con, stmt, rslt);
+        }
+    }
+
+    public void updateStudent(Student theStudent) throws Exception {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conn = dataSource.getConnection();
+            String sql = "UPDATE student SET first_name=?, last_name=?, email=? WHERE id=?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, theStudent.getFirstName());
+            stmt.setString(2, theStudent.getLastName());
+            stmt.setString(3, theStudent.getEmail());
+            stmt.setInt(4, theStudent.getId());
+            stmt.execute();
+        } 
+        finally {
+            close(conn, stmt, null);
+        }
+    }
 }
